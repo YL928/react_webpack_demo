ab116d6f375884bac47b2c26b173097821b469ee
yl928 <yl928@outlook.com>
2019-07-16 22:55:25 +0800

    success

---
+++ webpack.config.babel.js
@@ -0,0 +1,26 @@
+import jsxobj from 'jsxobj';
+
+// example of an imported plugin
+const CustomPlugin = config => ({
+  ...config,
+  name: 'custom-plugin'
+});
+
+export default (
+  <webpack target="web" watch mode="production">
+    <entry path="src/index.js" />
+    <resolve>
+      <alias {...{
+        react: 'preact-compat',
+        'react-dom': 'preact-compat'
+      }} />
+    </resolve>
+    <plugins>
+      <uglify-js opts={{
+        compression: true,
+        mangle: false
+      }} />
+      <CustomPlugin foo="bar" />
+    </plugins>
+  </webpack>
+);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:{
+    rules:[
+      {
+        test: /(\.jsx|\.js)$/,
+        use:[{
+            loader:"babel-loader"}],
+        exclude:/node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader'
+      }
+    ]
+  }
+}
